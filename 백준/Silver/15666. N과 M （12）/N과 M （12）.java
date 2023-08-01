f188673c754c0395ec701ab10a32dcc478ff6edb
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2023-08-01 16:46:03 +0900

    [Silver II] Title: N과 M (12), Time: 124 ms, Memory: 17864 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+
+	static int n;
+	static int m;
+
+	static int[] arr;
+	static boolean[] vis;
+	static int[] nums;
+
+	static Set<String> set = new HashSet<>();
+
+	public static void main(String[] args) throws IOException {
+
+		///////////////////// 구현부//////////////////////
+
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		arr = new int[m];
+		nums = new int[n];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++)
+			nums[i] = Integer.parseInt(st.nextToken());
+
+		Arrays.sort(nums);
+
+		func(0, 0);
+
+		System.out.print(sb);
+
+	}
+
+	public static void func(int idx, int k) {
+		if (k == m) {
+			String s = Arrays.toString(arr);
+			if (!set.contains(s)) {
+				for(int i = 0; i < m; i++)
+					sb.append(arr[i] + " ");
+				sb.append("\n");
+				set.add(Arrays.toString(arr));
+			}
+			return;
+		}
+
+		for (int i = idx; i < n; i++) {
+			arr[k] = nums[i];
+			func(i, k + 1);
+		}
+
+	}
+}
