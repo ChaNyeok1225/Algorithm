85cf9a68e1b44c40ce90798c0e72ee214d135921
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2023-08-02 22:28:41 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 748 ms, Memory: 140044 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -2,46 +2,51 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder sb = new StringBuilder();
 	static StringTokenizer st;
 
+
 	public static void main(String[] args) throws IOException {
-		
+
+		////////////////////// 구현부/////////////////////////
+
+
 		st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 		
-		int[][] arr = new int[n][n+1];
+		int[][] arr = new int[n+1][n+1];
 		
-		for(int i = 0; i < n; i++)
-			arr[i][0] = 0;
-		
-		for(int i = 0; i < n; i++) {
+		for(int i = 1; i < n+1; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j = 1; j < n+1; j++) {
-				arr[i][j] = arr[i][j-1] + Integer.parseInt(st.nextToken());
+				arr[i][j] = Integer.parseInt(st.nextToken())
+						+ arr[i-1][j]
+						+ arr[i][j-1] 
+						- arr[i-1][j-1] ;
 			}
 		}
-		int sx,sy,ex,ey;
 		int sum;
 		while(m-- > 0) {
 			sum = 0;
 			st = new StringTokenizer(br.readLine());
-			sx = Integer.parseInt(st.nextToken());
-			sy = Integer.parseInt(st.nextToken());
-			ex = Integer.parseInt(st.nextToken());
-			ey = Integer.parseInt(st.nextToken());
+			int sx = Integer.parseInt(st.nextToken());
+			int sy = Integer.parseInt(st.nextToken());
+			int ex = Integer.parseInt(st.nextToken());
+			int ey = Integer.parseInt(st.nextToken());
 			
-			for(int i = sx; i <= ex; i++) {
-				sum += arr[i-1][ey] - arr[i-1][sy-1];
-			}
+			sum = arr[ex][ey] - arr[sx-1][ey] - arr[ex][sy-1] + arr[sx-1][sy-1];
 			
-			sb.append(sum + "\n");
+			sb.append(sum+"\n");
+			
 		}
 		
-		System.out.print(sb);
+        System.out.print(sb);
+		
+		br.close();
 	}
-	
-	
+
+
 }
