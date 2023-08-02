3f3e2f321303a479563922dfaaf7dee84d6c7f65
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2023-08-02 13:18:34 +0900

    [Silver III] Title: N과 M (2), Time: 76 ms, Memory: 11556 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,64 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder sb = new StringBuilder();
 	static StringTokenizer st;
 
 	static int n;
 	static int m;
-	static boolean[] vis;
-
+	static int[] arr;
 	
-	
-
 	public static void main(String[] args) throws IOException {
-
-		////////////////////// 구현부/////////////////////////
-
+		
 		st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		
-		vis = new boolean[n];
-		func(0, 0);
-
-		System.out.println(sb);
-		////////////////////////////////////////////////////
-		br.close();
-	}
-
-	static void func(int idx, int cnt) {
-//		System.out.println(Arrays.toString(vis));
-		if (cnt == m) {
-			for(int i = 0; i < n; i++) {
-				if(vis[i] == true) {
-					sb.append((i+1) + " ");
-				}
-			}
-			sb.append("\n");
-			return;
-		}	
+		arr = new int[m];
 		
-		for(int i = idx; i < n; i++) {
-			if(vis[i] == true) continue;
+		re(1,0); 
+		System.out.print(sb);
+	}
+	
+	static void re(int idx, int cnt) {
+		if(cnt == m) {
+			for(int i = 0; i < m; i++)
+				sb.append(arr[i] + " ");
+			sb.append("\n");
 			
-			vis[i] = true;
-			func(i + 1, cnt + 1);
-			vis[i] = false;
+			return;
+		}
+		
+		for(int i = idx; i < n+1; i++) {
+			arr[cnt] = i;
+			re(i+1, cnt+1);
 			
 		}
 		
 	}
-
 }
