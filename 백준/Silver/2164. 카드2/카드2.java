74ba3d70951aada62d368ba0c307a69ddf34c6d9
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2023-08-04 16:56:37 +0900

    [Silver IV] Title: 카드2, Time: 128 ms, Memory: 23844 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,7 +1,6 @@
 import java.util.*;
 import java.io.*;
 
-
 public class Main {
 
 	public static void main(String[] args) throws IOException {
@@ -10,21 +9,24 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		////////////////////// 구현부/////////////////////////
-
+		
 		int n = Integer.parseInt(br.readLine());
 		
-		Deque<Integer> deque = new LinkedList<Integer>();
+		// 큐 생성
+		Deque<Integer> deque = new ArrayDeque<Integer>();
 		
+		// 큐 초기화
 		for(int i = 1; i < n + 1; i++)
 			deque.add(i);
 		
+		// 카드가 한 장 남을 때까지 순환
 		while (deque.size() != 1) {
 			deque.pollFirst();
 			deque.add(deque.pollFirst());
 		}	
 		
+		// 출력
 		System.out.println(deque.peekFirst());
-		
 		
 		////////////////////////////////////////////////////
 		br.close();
