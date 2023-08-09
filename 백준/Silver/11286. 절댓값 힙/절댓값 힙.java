3a75fe2efc17f83becfa435ad1a32b49c5c07469
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2023-08-09 15:25:41 +0900

    [Silver I] Title: 절댓값 힙, Time: 408 ms, Memory: 30300 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -10,27 +10,26 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 
 		int n = Integer.parseInt(br.readLine());
-		
-		PriorityQueue<Integer> q = new PriorityQueue<Integer>((n1, n2) ->{
-			if(Math.abs(n1) == Math.abs(n2)) return n1 - n2;
-			return Math.abs(n1) - Math.abs(n2);
-		}); 
-		
+
+		PriorityQueue<int[]> q = new PriorityQueue<>((int[] n1, int[] n2) -> {
+			if (n1[0] == n2[0])
+				return n1[1] - n2[1];
+			return n1[0] - n2[0];
+		});
+
 		while (n-- > 0) {
 			int ins = Integer.parseInt(br.readLine());
-			
-			if(ins == 0) {
-				if(q.isEmpty())
-					sb.append(0+"\n");
+
+			if (ins == 0) {
+				if (q.isEmpty())
+					sb.append(0 + "\n");
 				else
-					sb.append(q.poll() + "\n");
-			}
-			else
-				q.add(ins);
+					sb.append(q.poll()[1] + "\n");
+			} else
+				q.add(new int[] { Math.abs(ins), ins });
 		}
-		
+
 		System.out.print(sb);
 	}
-	
 
 }
