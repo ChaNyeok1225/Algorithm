5ce68e1bed298b5cfd7eab02ff39289edc030bb2
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2023-08-10 14:26:59 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 261 ms, Memory: 22,168 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -6,57 +6,61 @@ class Solution {
 	static StringBuilder sb = new StringBuilder();
 	static StringTokenizer st;
 
-	static int[][] ing = new int[20][20];
-	
-	
+	static int n;
+	static boolean[] group;
+	static int[][] taste;
+
+	static int bal;
+
 	public static void main(String[] args) throws IOException {
 
-		int T = Integer.parseInt(br.readLine()); // 테스트 케이스 횟수
+		int T = Integer.parseInt(br.readLine());
 
-		for (int tc = 1; tc < T + 1; tc++) { // T번 반복
-			
-			int n = Integer.parseInt(br.readLine()); // 재료의 개수
-			
-			// 재료 입력
-			for(int i = 0; i < n; i++) { 
+		for (int tc = 1; tc < T + 1; tc++) {
+
+			n = Integer.parseInt(br.readLine());
+
+			group = new boolean[n + 1];
+			taste = new int[n + 1][n + 1];
+			bal = Integer.MAX_VALUE;
+
+			for (int i = 1; i < n + 1; i++) {
 				st = new StringTokenizer(br.readLine());
-				for(int j = 0; j < n; j++)
-					ing[i][j] = Integer.parseInt(st.nextToken());
+				for (int j = 1; j < n + 1; j++)
+					taste[i][j] = Integer.parseInt(st.nextToken());
 			}
-			
-			// 부분집합 바이너리 카운팅 (A, B 요리를 선택하는 것이기 때문에 대칭 배제를 위해 n-1)
-			int noc = 1 << n-1;
-			int min = Integer.MAX_VALUE;
-			int tmp, sel;
-			int[] group = new int[2];
-		
-			for(int c = 0; c < noc; c++) { // 0 부터 noc까지
-				tmp = c;
-				group[0] = group[1] = 0; // 맛 초기화
-				for(int i = 0; i < n; i++) { // 재료 선택
-					
-					sel = (tmp & 1 << i) != 0? 1 : 0; // 어디 음식에 속하는지 비트 마스킹 검정
-					
-					for(int j = i + 1; j < n; j++) { // 다른 재료가 어느 음식에 속하는지 비트 마스킹 검정
-						
-						if(((tmp & 1 << j) != 0? 1 : 0) == sel) { // 같다면 그룹에 더해줌
-//							System.out.println(sel + "+ " + i + ", " + j + " = " +ing[i][j] + " " + ing[j][i]);
-							group[sel] += ing[i][j] + ing[j][i];
-						}
-						
+
+			func(0, 1, n / 2);
+			System.out.printf("#%d %d\n",tc,bal);
+		}
+	}
+
+	static void func(int cnt, int idx, int end) {
+		if (cnt == n / 2) {
+			int A = 0;
+			int B = 0;
+			for (int i = 1; i < n + 1; i++) {
+				for (int j = i; j < n + 1; j++) {
+					if (group[i] == group[j]) {
+						if (group[i])
+							A += taste[i][j] + taste[j][i];
+						else
+							B += taste[i][j] + taste[j][i];
 					}
 				}
-				
-//				System.out.println(group[0] + " vs " + group[1]);
-				
-				if(Math.abs(group[0] - group[1]) < min)
-					min = Math.abs(group[0] - group[1]);
-				
 			}
-			
-			System.out.printf("#%d %d\n",tc,min);
+
+			int b = Math.abs(A - B);
+			if (b < bal)
+				bal = b;
+
+			return;
 		}
 
+		for (int i = idx; i < end; i++) {
+			group[i] = true;
+			func(cnt + 1, i + 1, n + 1);
+			group[i] = false;
+		}
 	}
-	
 }
