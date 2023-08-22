8c983a9a0321829958a60240ff0b5ac4f31ad171
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2023-08-22 11:31:40 +0900

    [Unrated] Title: [모의 SW 역량테스트] 줄기세포배양, Time: 378 ms, Memory: 79,772 KB -BaekjoonHub

--- SWEA/Unrated/5653. ［모의 SW 역량테스트］ 줄기세포배양/［모의 SW 역량테스트］ 줄기세포배양.java
+++ SWEA/Unrated/5653. ［모의 SW 역량테스트］ 줄기세포배양/［모의 SW 역량테스트］ 줄기세포배양.java
@@ -8,9 +8,8 @@ public class Solution {
 	static StringTokenizer st;
 
 	static final int SIZE = 400;
-	static int[][] board, time;
+	static int[][] board;
 	static int[] dx = { 1, 0, -1, 0 }, dy = { 0, 1, 0, -1 };
-	static PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
 
 	public static void main(String[] args) throws IOException {
 		int T = Integer.parseInt(br.readLine());
@@ -22,64 +21,49 @@ public class Solution {
 			int k = Integer.parseInt(st.nextToken());
 
 			board = new int[SIZE][SIZE];
-			time = new int[SIZE][SIZE];
-			boolean[][] chk = new boolean[SIZE][SIZE];
 			int cnt = 0;
-			
-			pq.clear();
-			
-			for (int i = SIZE/2; i < SIZE/2 + n; i++) {
+
+			PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+				if (a[2] == b[2])
+					return b[3] - a[3];
+				return a[2] - b[2];
+			});
+
+			for (int i = SIZE / 2; i < SIZE / 2 + n; i++) {
 				st = new StringTokenizer(br.readLine());
-				for (int j = SIZE/2; j < SIZE/2 + m; j++) {
+				for (int j = SIZE / 2; j < SIZE / 2 + m; j++) {
 					board[i][j] = Integer.parseInt(st.nextToken());
-					
+
 					if (board[i][j] != 0) {
-						time[i][j] = 1;
 						pq.offer(new int[] { i, j, board[i][j], board[i][j] });
-						if(2*board[i][j] > k && !chk[i][j]) {
-							chk[i][j] = true;
+						if (2 * board[i][j] > k)
 							cnt++;
-						}
 					}
 				}
 			}
 			int sec = 0;
-
 			while (sec <= k) {
-				
+
 				if (pq.peek()[2] >= sec) {
 					sec++;
 					continue;
 				}
-
 				int[] p = pq.poll();
-				
-				if(board[p[0]][p[1]] != p[3])
-					continue;
-				
+
 				for (int dir = 0; dir < 4; dir++) {
 					int nx = p[0] + dx[dir];
 					int ny = p[1] + dy[dir];
-					
-					if((time[nx][ny] == 0 || time[nx][ny] == sec) && board[nx][ny] < p[3]) {
+
+					if (board[nx][ny] == 0) {
 						board[nx][ny] = p[3];
-						time[nx][ny] = sec;
-						
-						pq.offer(new int[] {nx, ny, sec + p[3], p[3]});
-						if(sec + p[3] + p[3] > k && !chk[nx][ny]) {
-							chk[nx][ny] = true;
+						pq.offer(new int[] { nx, ny, sec + p[3], p[3] });
+						if (sec + p[3] * 2 > k)
 							cnt++;
-						}
-						
 					}
-					
 				}
 			}
-			
-//			for(int i = 0; i < SIZE; i++)
-//				System.out.println(Arrays.toString(chk[i]));
-			
-			System.out.printf("#%d %d\n",tc, cnt);
+
+			System.out.printf("#%d %d\n", tc, cnt);
 		}
 	}
 
