d088c50c29a16dcc70d4a67a5e3103a507953a6e
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2023-08-29 11:41:54 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 280 ms, Memory: 59688 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -11,49 +11,29 @@ public class Main {
 		int m = Integer.parseInt(st.nextToken());
 
 		int[][] board = new int[n + 1][m + 1];
-		int[][][] D = new int[n + 1][m + 1][3];
-
-		for (int i = 1; i < n + 1; i++) {
-			char[] ch = br.readLine().toCharArray();
-			for (int j = 1; j < m + 1; j++) {
-				board[i][j] = ch[j - 1] - '0';
-				if (board[i][j] != 0) {
-					D[i][j][0] = 1;
-					D[i][j][1] = 1;
-					D[i][j][2] = 1;
-				}
-			}
-		}
+		int[][] D = new int[n + 1][m + 1];
 
 		int max = 0;
-		for (int i = 1; i < n + 1; i++) {
-
-			for (int j = 1; j < m + 1; j++) {
-				if (D[i][j][0] > 0) {
-					int size = 1000;
-					if (D[i - 1][j][0] > 0)
-						D[i][j][0] += D[i - 1][j][0];
-					if (D[i][j - 1][1] > 0)
-						D[i][j][1] += D[i][j - 1][1];
-					if(D[i-1][j-1][2] > 0)
-						D[i][j][2] += min(D[i-1][j-1][0], D[i-1][j-1][1],D[i-1][j-1][2]);
-					
-					size = min(D[i][j][0],D[i][j][1],D[i][j][2]);
-					
-					if(max < size)
-						max = size;
-				}
+		for (int i = 0; i < n; i++) {
+			char[] ch = br.readLine().toCharArray();
+			for (int j = 0; j < m; j++) {
+				board[i][j] = ch[j] - '0';
+				D[i][j] = board[i][j];
+				if(board[i][j] > 0)
+					max = 1;
 			}
 		}
 
-//		for (int i = 0; i < n + 1; i++) {
-//			for (int j = 0; j < m + 1; j++)
-//				System.out.print(Arrays.toString(D[i][j]) + " ");
-//			System.out.println();
-//		}
-		
+		for (int i = 1; i < n; i++) {
+			for (int j = 1; j < m; j++) {
+				if (board[i][j] > 0) {
+					D[i][j] = min(D[i][j-1], D[i-1][j],D[i-1][j-1]) + 1;
+					if(max < D[i][j])
+						max = D[i][j];
+				}
+			}
+		}
 		System.out.println(max*max);
-		
 	}
 
 	static int min(int... num) {
@@ -61,9 +41,6 @@ public class Main {
 		for(int i = 0; i < num.length; i++)
 			if(num[i] < a)
 				a= num[i];
-		
 		return a;
 	}
-	
-	
 }
