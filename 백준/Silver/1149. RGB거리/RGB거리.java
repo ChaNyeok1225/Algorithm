e10d353ef102295f7e20dd39dcf09dd56a6289dd
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2023-08-29 17:23:21 +0900

    [Silver I] Title: RGB거리, Time: 84 ms, Memory: 12124 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,42 +1,45 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
-
-	
 	public static void main(String[] args) throws IOException {
-
-		int n = Integer.parseInt(br.readLine());
 		
-		int[][] H = new int[n][3];
-		for(int i = 0; i < n; i++) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine()); // 집의 수 n
+		
+		int[][] cost = new int[n][3]; // 0 ~ n-1 번의 집을 빨강, 초록, 파랑으로 칠하는 비용
+		
+		for(int  i = 0; i < n; i++) { // 비용 입력
 			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < 3; j++)
-				H[i][j] = Integer.parseInt(st.nextToken());
+			cost[i][0] = Integer.parseInt(st.nextToken());
+			cost[i][1] = Integer.parseInt(st.nextToken());
+			cost[i][2] = Integer.parseInt(st.nextToken());
 		}
 		
+		// [0: 빨강으로, 1: 초록으로, 2: 파랑으로 ] 칠 했을 떄의 최솟 값
 		int[][] D = new int[n][3];
 		
-		D[0][0] = H[0][0];
-		D[0][1] = H[0][1];
-		D[0][2] = H[0][2];
+		// 0번째 집을 칠 했을 때의 비용 지정
+		D[0][0] = cost[0][0]; // 빨간색으로
+		D[0][1] = cost[0][1]; // 초록색으로
+		D[0][2] = cost[0][2]; // 파란색으로
 		
-		for(int i = 1; i < n; i++) {
-			D[i][0] = H[i][0] + Math.min(D[i-1][1], D[i-1][2]);
-			D[i][1] = H[i][1] + Math.min(D[i-1][0], D[i-1][2]);
-			D[i][2] = H[i][2] + Math.min(D[i-1][0], D[i-1][1]);
+		for(int i = 1; i < n; i++) { // 1번 집부터 n-1집까지
+			// 해당 색으로 칠하는 비용 + 나와 다른 색을 가진 집 중 최소를 선택
+			D[i][0] = Math.min(D[i-1][1], D[i-1][2]) + cost[i][0]; 
+			D[i][1] = Math.min(D[i-1][0], D[i-1][2]) + cost[i][1];
+			D[i][2] = Math.min(D[i-1][0], D[i-1][1]) + cost[i][2];
 		}
-
-		int res = Math.min(D[n-1][0], D[n-1][1]);
-		res = Math.min(res, D[n-1][2]);
 		
-		System.out.println(res);
-		
-		
+		// 각 색중 가장 비용이 적게든 값 선택하여 출력
+		int min = Integer.MAX_VALUE;
+		for(int i = 0; i < 3; i++)
+			if(D[n-1][i] < min)
+				min = D[n-1][i];
+		System.out.println(min);
 	}
-
 }
