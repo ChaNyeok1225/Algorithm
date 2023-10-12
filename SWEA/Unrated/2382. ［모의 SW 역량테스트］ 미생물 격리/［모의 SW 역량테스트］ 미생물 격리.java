c82fce8d344cb63f2e2512bcb9991bb4fb79f49b
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2023-10-12 10:38:48 +0900

    [Unrated] Title: [모의 SW 역량테스트] 미생물 격리, Time: 339 ms, Memory: 56,332 KB -BaekjoonHub

--- SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
+++ SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
@@ -1,119 +1,118 @@
 import java.io.*;
 import java.util.*;
 
-class Solution {
+public class Solution {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+
+	static class Cell {
+		int x;
+		int y;
+		int num;
+		int dir;
+
+		public Cell(int x, int y, int num, int dir) {
+			super();
+			this.x = x;
+			this.y = y;
+			this.num = num;
+			this.dir = dir;
+		}
+
+	}
+
+	static List<Cell> cells;
 
 	static int[] dx = { -1, 1, 0, 0 }, dy = { 0, 0, -1, 1 };
+	static int n, board[][], tmp[][];
 
-	public static void main(String[] args) throws IOException {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+	public static void main(String[] args) throws Exception {
 
 		int T = Integer.parseInt(br.readLine());
 
 		for (int tc = 1; tc < T + 1; tc++) {
 
 			st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken());
+			n = Integer.parseInt(st.nextToken());
 			int m = Integer.parseInt(st.nextToken());
 			int k = Integer.parseInt(st.nextToken());
 
-			int[][] board = new int[n][n];
-			int[][] tmp = new int[n][n];
-			HashMap<Integer, int[]> hm = new HashMap<>();
-			hm.put(0,new int[] {0,0,0,0});
-			for (int i = 1; i < k + 1; i++) {
+			cells = new ArrayList<Cell>();
+			board = new int[n][n];
+			tmp = new int[n][n];
+			for (int i = 0; i < n; i++) {
+				Arrays.fill(board[i], -1);
+				Arrays.fill(tmp[i], -1);
+			}
+
+			for (int i = 0; i < k; i++) {
 
 				st = new StringTokenizer(br.readLine());
 				int x = Integer.parseInt(st.nextToken());
 				int y = Integer.parseInt(st.nextToken());
-				int nums = Integer.parseInt(st.nextToken());
-				int dir = Integer.parseInt(st.nextToken());
+				int num = Integer.parseInt(st.nextToken());
+				int dir = Integer.parseInt(st.nextToken()) - 1;
 
+				cells.add(new Cell(x, y, num, dir));
 				board[x][y] = i;
-				hm.put(i, new int[] { x, y, nums, dir - 1, nums });
 			}
 
-//			System.out.println(0 + " :: --");
-//			for(int i = 0 ; i < n; i++) {
-//				for(int j = 0; j < n; j++) {
-//					System.out.printf("%5d",hm.get(board[i][j])[2]);
-//				}
-//				System.out.println();
-//			}
+			for (int time = 0; time < m; time++) 
+				move();
 			
-			for (int sec = 0; sec < m; sec++) {
+			int ans = 0;
+			for(Cell c : cells) 
+				ans += c.num;
+			
+			sb.append(String.format("#%d %d\n", tc, ans));
+		}
+		System.out.print(sb);
 
-				
-				for (int i = 0; i < n; i++)
-					Arrays.fill(tmp[i], 0);
+	}
 
-				for (int cell = 1; cell < k + 1; cell++) {
-					if (!hm.containsKey(cell))
-						continue;
-					int[] c = hm.get(cell);
+	static void move() {
 
-					c[0] += dx[c[3]];
-					c[1] += dy[c[3]];
+		Collections.sort(cells, (a, b) -> b.num - a.num);
 
-					if (c[0] < 1 || c[1] < 1 || n - 1 <= c[0] || n - 1 <= c[1]) {
-						c[2] /= 2;
-						c[4] /= 2;
-						c[3] ^= 1;
+		for (int i = 0; i < cells.size(); i++) {
 
-						if (c[2] == 0) {
-							hm.remove(cell);
-							continue;
-						}
-					}
+			Cell cur = cells.get(i);
 
-					if (tmp[c[0]][c[1]] != 0) {
-						int[] ac = hm.get(tmp[c[0]][c[1]]);
+			int nx = cur.x + dx[cur.dir];
+			int ny = cur.y + dy[cur.dir];
 
-						if (ac[4] > c[4]) {
-							ac[2] += c[2];
-							hm.remove(cell);
-						} else {
-							c[2] += ac[2];
-							hm.remove(tmp[c[0]][c[1]]);
-							tmp[c[0]][c[1]] = cell;
-						}
-
-					} else
-						tmp[c[0]][c[1]] = cell;
-
-				}
-
-				for (int i = 0; i < n; i++)
-					for (int j = 0; j < n; j++)
-						board[i][j] = tmp[i][j];
-				
-				for(int i = 1; i < k +1; i++) {
-					if(hm.containsKey(i)) {
-						int[] c= hm.get(i);
-						c[4] = c[2];
-					}
-				}
-				
-//				System.out.println(sec+1 + " :: --");
-//				for(int i = 0 ; i < n; i++) {
-//					for(int j = 0; j < n; j++) {
-//						System.out.printf("%5d",hm.get(board[i][j])[2]);
-//					}
-//					System.out.println();
-//				}
-				
+			if (tmp[nx][ny] != -1) {
+				cells.get(tmp[nx][ny]).num += cur.num;
+				cells.remove(i);
+				i--;
+				continue;
 			}
 
-			int total = 0;
-			for (int i = 1; i < k + 1; i++)
-				if (hm.containsKey(i)) {
-//					System.out.println(Arrays.toString(hm.get(i)));
-					total += hm.get(i)[2];
+			cur.x = nx;
+			cur.y = ny;
+			tmp[nx][ny] = i;
+			if (nx == 0 || nx == n - 1 || ny == 0 || ny == n - 1) {
+				cur.num /= 2;
+				cur.dir ^= 1;
+				if (cur.num == 0) {
+					cells.remove(i);
+					tmp[nx][ny] = -1;
+					i--;
 				}
+			}
 
-			System.out.printf("#%d %d\n", tc, total);
+		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				board[i][j] = tmp[i][j];
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			Arrays.fill(tmp[i], -1);
 		}
 
 	}
