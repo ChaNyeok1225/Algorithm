654602c64f164991572a11733b1b627c3a56d6d1
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2023-10-23 20:31:35 +0900

    [Silver III] Title: 빈도 정렬, Time: 232 ms, Memory: 18500 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+import java.util.Map.Entry;
 
 public class Main {
 
@@ -9,44 +10,35 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 
-		HashMap<Integer,int[]> map = new HashMap<>();
-		
+		HashMap<Integer, Integer> map = new LinkedHashMap<>();
+
 		st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int c = Integer.parseInt(st.nextToken());
-		
+
 		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < n ; i++) {
+		for (int i = 0; i < n; i++) {
 			int val = Integer.parseInt(st.nextToken());
-			int[] cur = map.getOrDefault(val, new int[] {0, i});
-			cur[0]++;
-			map.put(val, cur);
+			map.put(val, map.getOrDefault(val, 0) + 1);
 		}
-			
-		int[][] arr = new int[map.size()][3];
-		
+
+		int[][] arr = new int[map.size()][2];
+
 		int cnt = 0;
-		for(Integer key : map.keySet()) {
-			arr[cnt][0] = key;
-			int[] tmp = map.get(key);
-			arr[cnt][1] = tmp[0];
-			arr[cnt][2] = tmp[1];
+		for (Entry<Integer, Integer> entry : map.entrySet()) {
+			arr[cnt][0] = entry.getKey();
+			arr[cnt][1] = entry.getValue();
 			cnt++;
 		}
-		
-		Arrays.sort(arr, (a, b) -> {
-			if(b[1] == a[1])
-				return a[2] - b[2];
-			return b[1]-a[1];
-		});
-		
-		for(int i = 0; i < arr.length; i++) {
-			for(int j = 0; j < arr[i][1]; j++)
+
+		Arrays.sort(arr, (a, b) -> b[1] - a[1]);
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i][1]; j++)
 				sb.append(arr[i][0]).append(" ");
 		}
-		
+
 		System.out.println(sb);
-		
 	}
 
 }
