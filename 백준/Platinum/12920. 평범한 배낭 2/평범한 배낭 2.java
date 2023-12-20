bb73837c2b2a5f76e9103742909041ff7828e3e8
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2023-12-20 16:56:28 +0900

    [Platinum IV] Title: 평범한 배낭 2, Time: 164 ms, Memory: 19408 KB -BaekjoonHub

---
+++ 백준/Platinum/12920. 평범한 배낭 2/평범한 배낭 2.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+//start	2023. 12. 20  16 : 42
+//end 	2023. 12. 20  16 : 56
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        int[][] use = new int[m+1][n+1];
+        int[] dp = new int[m+1];
+        
+        int[][] obj = new int[n+1][3];
+        for(int i = 1; i < n + 1; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	obj[i][0] = Integer.parseInt(st.nextToken());
+        	obj[i][1] = Integer.parseInt(st.nextToken());
+        	obj[i][2] = Integer.parseInt(st.nextToken());
+        }
+        
+        for(int i = 1; i < m + 1; i++) {
+        	int max = 0;
+        	int selidx = -1;
+        	
+        	dp[i] = dp[i-1];
+        	for(int j = 0; j < n + 1; j++)
+        		use[i][j] = use[i-1][j];
+        	
+        	for(int j = 1; j < n + 1; j++) {
+        		if(i < obj[j][0]) continue;
+        		
+        		if(max < dp[i-obj[j][0]] + obj[j][1] && use[i-obj[j][0]][j] < obj[j][2]) {
+        			max = dp[i-obj[j][0]] + obj[j][1];
+        			selidx = j;
+        		}
+        	}
+        	
+        	if(selidx != -1 && dp[i] < max) {
+        		for(int j = 1; j < n + 1; j++) {
+        			use[i][j] = use[i - obj[selidx][0]][j];
+        		}
+        		use[i][selidx]++;
+        		dp[i] = max;
+        	}
+        }
+        
+        System.out.println(dp[m]);
+    }
+}
