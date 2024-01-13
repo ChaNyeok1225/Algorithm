30bdf4fd07985fdea9e9754b4b30125a8b02be27
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-01-13 14:36:25 +0900

    [level Lv.1] Title: 실패율, Time: 0.51 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/Lv.1/42889. 실패율/실패율.java
+++ 프로그래머스/Lv.1/42889. 실패율/실패율.java
@@ -1,34 +1,76 @@
-import java.util.*;
+import java.util.*; 
+// 실패율 : 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어 수
+// N : 전체 스테이지 개수 N, 
+// 게임을 이용하는 사용자가 현재 멈춰있는 스테이지의 번호가 담긴 배열 stages
+// 실패율이 높은 스테이지부터 내림차순으로 스테이지의 번호가 담겨있는 배열
 
 class Solution {
+    static class Data implements Comparable<Data>{
+        double rate; 
+        int num; 
+        
+        public Data(double rate, int num){
+            this.rate = rate;
+            this.num = num; 
+        }
+        
+        @Override
+        public String toString(){
+            return "rate : " + rate + " num : " + num; 
+        }
+        
+        @Override
+        public int compareTo(Data data) {
+            if(this.rate == data.rate){
+                return this.num - data.num; 
+            }
+            return Double.compare(data.rate, this.rate);
+        }
+    }
+    
     public int[] solution(int N, int[] stages) {
-        int[] answer = new int[N];
+        int person = stages.length; // 사람 수
         
-        int[] clear = new int[N+1];
-        for(int i = 0;  i < stages.length; i++) {
-            clear[stages[i]-1]++;
-        }
+        int[] pass = new int[N+2];
+        int[] now = new int[N+2]; 
         
-        
-        int total = stages.length;
-        
-        int c = 0;
-        double[][] ratio = new double[N][2];
-        for(int i = 0; i < N; i++) {
-            System.out.println( clear[i] + " , " +total);
-            if(total == 0)    
-                ratio[i][0] = 0;
-            else
-                ratio[i][0] = (double)clear[i] / total;
+        // 사람별 어디있는지.. 
+        for(int i : stages){
+            // System.out.println("i :"+ i); 
+            now[i]++; 
             
-            ratio[i][1] = i+1;
-            total -= clear[i];
+            for(int j = i-1; j > 0; j--){
+                // System.out.println("j :"+ j); 
+                pass[j]++; 
+            }
+            // System.out.println("pass : " + Arrays.toString(pass));
+            // System.out.println("nows : " + Arrays.toString(now)); 
         }
         
+    
+        // 실패율을 저장할 자료구조
+        PriorityQueue<Data> pq = new PriorityQueue<>(); 
         
-        Arrays.sort(ratio, (a,b) -> Double.compare(b[0],a[0]));
-        for(int i = 0; i < N; i++)
-            answer[i] = (int)ratio[i][1];
+        // TreeMap<Integer, Integer> map = new TreeMap<>();
+        
+        for(int i=1; i<N+1; i++){
+            
+            double rate; 
+            if(pass[i] + now[i] == 0){ // 다 통과한 경우
+                rate = 0; 
+            }else{
+                rate = (double) now[i] / (double) (now[i] + pass[i]); // 계산?   
+            }
+            
+            pq.add(new Data(rate, i)); 
+        }
+        
+        int[] answer = new int[pq.size()];
+        int size = pq.size();
+        for(int i=0; i<size; i++){
+            answer[i] = pq.poll().num; 
+        }
+        
         return answer;
     }
 }
