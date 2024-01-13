0bc2f9d512e18033b32f39a9c03fb9fa39600a80
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-01-13 13:04:24 +0900

    [level Lv.2] Title: 후보키, Time: 5.24 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/Lv.2/42890. 후보키/README.md
+++ 프로그래머스/Lv.2/42890. 후보키/README.md
@@ -1,10 +1,10 @@
 # [level Lv.2] 후보키 - 42890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42890#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42890) 
 
 ### 성능 요약
 
-메모리: 74.9 MB, 시간: 1.15 ms
+메모리: 76.5 MB, 시간: 5.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 6일 12:29:28
+2024년 1월 6일 13:4:23
 
 ### 문제 설명
 
--- 프로그래머스/Lv.2/42890. 후보키/후보키.java
+++ 프로그래머스/Lv.2/42890. 후보키/후보키.java
@@ -2,82 +2,65 @@ import java.util.*;
 
 class Solution {
     
-    static int sel[], n, m, answer;
-    static List<String> list = new ArrayList<>();
-    static Queue<Integer> q = new ArrayDeque<>();
-
     public int solution(String[][] relation) {
         
-        n = relation[0].length;
-        m = relation.length;
-        sel = new int[n]; 
-        dfs(0, 0, 0, relation);        
+        int n = relation.length;
+        int m = relation[0].length;
         
-        int answer = 0;
+        Queue<Integer> q = new ArrayDeque<>();
+        
+        for(int i = 0; i < m; i++)
+            q.offer(1 << i);
         
         List<Integer> min = new ArrayList<>();
         
-        Collections.sort(list, (a,b) -> a.length() - b.length());
-        
-        boolean chk;
-        int flag;
-        for(int i = 0; i < list.size(); i++) {
-            flag = 0;
-            char[] cur = list.get(i).toCharArray();
-            for(int j = 0; j < cur.length; j++) 
-                flag |= (1<<(cur[j]-'0'));
+        boolean[] sel = new boolean[m];
+        loop: while(!q.isEmpty()) {
+            int cur = q.poll();
             
-            chk = true;
-            for(int j = 0; j < min.size(); j++) {
-                if((min.get(j) & flag) == min.get(j)) {
-                    chk = false;
-                    break;
-                }
+            for(int i = 0; i < min.size(); i++) {
+                if((min.get(i) & cur) == min.get(i))
+                    continue loop;
             }
             
-            if(chk)
-                min.add(flag);
+            Arrays.fill(sel, false);
+            for(int i = 0; i < m; i++) {
+                if((cur & (1 << i)) != 0)
+                    sel[i] = true;
+            }
+            
+			boolean chk = false;
+			uniq : for (int i = 0; i < n - 1; i++) {
+				for (int j = i + 1; j < n; j++) {
+					chk = false;
+					for (int k = 0; k < m; k++) {
+						if (!sel[k])
+							continue;
+
+						if (!relation[i][k].equals(relation[j][k])) {
+							chk = true;
+						}
+					}
+					
+					if (!chk)
+						break uniq;
+				}
+			}
+
+            
+            if(chk) {
+                min.add(cur);
+            } else {
+                for(int i = 0; i < m; i++) {
+                    if(sel[i]) continue;
+                    q.offer(cur | (1<<i));
+                }
+            }
             
         }
         
         
         return min.size();
-    }
-    
-    static StringBuilder sb = new StringBuilder();
-    static void dfs(int idx, int cnt, int flag, String[][] relation) {
-        if(idx == n) {
-            if(cnt == 0)
-                return;
-            
-            int index = 0;
-            sb.setLength(0);
-            for(int i = 0; i < n; i++) {
-                if((flag & (1<<i)) != 0) {
-                    sel[index++] = i;
-                    sb.append(i);
-                }
-            }
-            
-            boolean rchk;
-            for(int i = 0; i < m - 1; i++) {
-                for(int j = i + 1; j < m; j++) {
-                    rchk = false;
-                    for(int k = 0; k < cnt; k++) {
-                        if(!relation[i][sel[k]].equals(relation[j][sel[k]]))
-                            rchk = true;
-                    }
-                    if(!rchk)
-                        return;
-                }
-            }
-            list.add(sb.toString());
-            return;
-        }
-        
-        dfs(idx + 1, cnt, flag, relation);
-        dfs(idx + 1, cnt+1, flag | (1<<idx), relation);
-        
     }
 
 }
