2abf74598f40c8bf00afd4d345012cea07e2f3fa
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-04-11 00:34:29 +0900

    [level 3] Title: 거스름돈, Time: 9.03 ms, Memory: 52.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    
+    public int solution(int n, int[] money) {
+        
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        
+        
+        for(int i = 0; i < money.length; i++) {
+            for(int j = money[i]; j <= n; j++) {
+                dp[j] += dp[j - money[i]];
+            }
+        }
+        
+        return dp[n];
+        
+    }
+}
