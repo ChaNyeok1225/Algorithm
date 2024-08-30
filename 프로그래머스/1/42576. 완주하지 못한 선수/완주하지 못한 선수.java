a9d136a592fe7954c755a38e3bed95f37f9d5f9a
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-08-30 09:24:14 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 284.77 ms, Memory: 95.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,14 +2,18 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
         Arrays.sort(participant);
         Arrays.sort(completion);
         
-        for(int i = 0; i < completion.length; i++) {
-            if(!participant[i].equals(completion[i])) {
-                return participant[i];
+        int idx = 0;
+        for(; idx < completion.length; idx++) {
+            if(!participant[idx].equals(completion[idx])) {
+                break;
             }
         }
-        return participant[participant.length - 1];
+        
+        return participant[idx];
     }
 }
