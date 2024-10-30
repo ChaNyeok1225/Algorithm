b02497dbde3aa52066c9c526f7a7b5957105bbe7
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-10-30 21:26:41 +0900

    [level 3] Title: 부대복귀, Time: 182.32 ms, Memory: 173 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -2,41 +2,44 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
+        int len = sources.length;
+        int[] answer = new int[len];
         
-        List<Integer>[] graph = new ArrayList[n+1];
-        for(int i = 1; i < n + 1; i++)
+        ArrayList<Integer>[] graph = new ArrayList[n+1];
+        for(int i = 1; i < n + 1; i++) {
             graph[i] = new ArrayList<>();
-        for(int i = 0; i < roads.length; i++) {
-            graph[roads[i][0]].add(roads[i][1]);
-            graph[roads[i][1]].add(roads[i][0]);
         }
         
-        PriorityQueue<int[]> q = new PriorityQueue<>((a,b) -> a[1] - b[1]);
-        
-        int[] dist = new int[n+1];
-        Arrays.fill(dist, 150000);
-        dist[destination] = 0;
-        boolean[] vis = new boolean[n+1];
-        
-        q.offer(new int[] {destination, dist[destination]});
-        
-        while(!q.isEmpty()) {
-            int[] cur = q.poll();
-            if(vis[cur[0]]) continue;
-            vis[cur[0]] = true;
+        for(int[] edge : roads) {
+            graph[edge[1]].add(edge[0]);
+            graph[edge[0]].add(edge[1]);
+        }
             
-            for(int nxt : graph[cur[0]]) {
-                if(dist[nxt] <= cur[1] + 1) continue;
-                dist[nxt] = cur[1] + 1;
-                q.offer(new int[] {nxt, dist[nxt]});
-            }
-        }
+        ArrayDeque<Integer> q = new ArrayDeque<>();            
         
-        int[] answer = new int[sources.length];
-        for(int i =0 ; i < sources.length; i++) {
-            answer[i] = dist[sources[i]];
-            if(answer[i] == 150000)
-                answer[i] = -1;
+        int[] cost = new int[n+1];
+        Arrays.fill(cost, -1);
+        
+        q.offer(destination);
+        cost[destination] = 0;
+        
+        int cur, size, step = 0;
+        while(!q.isEmpty()) {
+            
+            size = q.size();
+            step++;
+            for(int i = 0; i < size; i++) {
+                cur = q.poll();
+                for(int nxt : graph[cur]) {
+                    if(cost[nxt] != -1) continue;
+                    cost[nxt] = step;
+                    q.offer(nxt);
+                }
+            }
+        }        
+        
+        for(int i = 0; i < len; i++) {
+            answer[i] = cost[sources[i]];
         }
         
         return answer;
