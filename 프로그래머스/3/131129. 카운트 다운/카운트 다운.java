e961a3b623d9d1fe09dec80c1d164dc69d63d3a8
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2024-11-21 21:55:49 +0900

    [level 3] Title: 카운트 다운, Time: 64.64 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
+++ 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
@@ -1,67 +1,35 @@
-// 키워드
-
-// - 점수를 깎아 정확히 0점으로 만들기
-// - 0점보다 낮아지면 버스트 실격
-// - 1 ~ 20 싱글, 더블, 트리플 각각의 배수, 그리고 50점 
-// - 먼저 0점, 같은 라운드면 싱글이나 불 많이 던지기
-
-// 접근법
-// - 걍 bfs하면 될거같은데?
-
+// DP 풀이
 
 import java.util.*;
 
 class Solution {
     public int[] solution(int target) {
-        int[] answer = new int[2];
-        
-        int[] count = new int[target];
-        int[] number = new int[target];
-        Arrays.fill(count, -1);
-        ArrayDeque<int[]> q = new ArrayDeque<>();
-        q.offer(new int[] {target, 0, 0});
-        
+        int[][] dp = new int[target+100][2];
         int num;
-        int[] cur;
-        while(!q.isEmpty()) {
-            cur = q.poll();
+        for(int i = 0; i < target; i++) {
             
-            if(cur[0] == 0) {
-                return new int[] {cur[1], count[0]};
+            num = i + 50;
+            if(dp[num][0] == 0 || dp[num][0] > dp[i][0] + 1) {
+                dp[num][0] = dp[i][0] + 1;
+                dp[num][1] = dp[i][1] + 1;
+            } else if(dp[num][0] == dp[i][0] + 1){
+                dp[num][1] = Math.max(dp[num][1], dp[i][1] + 1);
             }
             
-            num = cur[0] - 50;
-            if(num >= 0) {
-                if(number[num] == cur[1] + 1) 
-                    count[num] = Math.max(count[num], cur[2] + 1);
-                
-                if(count[num] == -1) {
-                    number[num] = cur[1] + 1;
-                    count[num] = cur[2] + 1;
-                    q.offer(new int[] {num, cur[1] + 1, count[num]});
+            for(int m = 1; m < 4; m++) {
+                for(int s = 1; s < 21; s++) {
+                    num = i + m * s;
+                    
+                    if(dp[num][0] == 0 || dp[num][0] > dp[i][0] + 1) {
+                        dp[num][0] = dp[i][0] + 1;
+                        dp[num][1] = dp[i][1] + (m == 1 ? 1 : 0);
+                    } else if(dp[num][0] == dp[i][0] + 1){
+                        dp[num][1] = Math.max(dp[num][1], dp[i][1] + (m == 1 ? 1 : 0));
+                    }
                 }
             }
-            
-            for(int i = 1; i < 4; i++) {
-                for(int j = 1; j < 21; j++) {
-                    num = cur[0] - (i * j);
-                    if(num >= 0) {
-                        if(number[num] == cur[1] + 1) 
-                            count[num] = Math.max(count[num], cur[2] + (i == 1 ? 1 : 0));
-                        
-                        if(count[num] == -1) {
-                            number[num] = cur[1] + 1;
-                            count[num] = cur[2] + (i == 1 ? 1 : 0);
-                            q.offer(new int[] {num, cur[1] + 1, count[num]});
-                        }
-                    } 
-                }
-            }
-            
-            
         }
         
-        
-        return answer;
+        return new int[] {dp[target][0], dp[target][1]};
     }
 }
