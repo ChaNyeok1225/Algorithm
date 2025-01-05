0ac42f7eea1f11d77fe885fe1d24fe9254bf78c6
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2025-01-05 21:43:35 +0900

    [Silver I] Title: 보석 상자, Time: 172 ms, Memory: 3192 KB -BaekjoonHub

---
+++ 백준/Silver/2792. 보석 상자/보석 상자.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+
+int N, M;
+int jewel[300001];
+
+int main() {
+	
+	cin >> N >> M;
+
+	int l = 1, r = 1;
+	for (int i = 0; i < M; i++) {
+		cin >> jewel[i];
+		r = r > jewel[i] ? r : jewel[i];
+	}
+
+	int answer = 0;
+	int mid, cnt;
+
+	while (l <= r) {
+		mid = (l + r) / 2;
+		cnt = 0;
+		for (int i = 0; i < M; i++) {
+			cnt += ((jewel[i] - 1) / mid) + 1;
+		}
+
+		if (cnt <= N) {
+			r = mid - 1;
+			answer = mid;
+		}
+		else {
+			l = mid + 1;
+		}
+	}
+
+	cout << answer;
+
+}
