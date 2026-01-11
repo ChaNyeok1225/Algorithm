190caad93366490869503cbf592df83c1c754283
ChaNyeok1225 <93437137+ChaNyeok1225@users.noreply.github.com>
2026-01-11 20:13:43 +0900

    [Gold I] Title: 이항 계수 3, Time: 16 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Gold/11401. 이항 계수 3/이항 계수 3.cc
+++ 백준/Gold/11401. 이항 계수 3/이항 계수 3.cc
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 using ll = long long;
-constexpr int SZ = 4'000'000;
 constexpr ll MOD = 1'000'000'007;
 
 int N, K;
 
-ll pow(ll a, ll b) {
+ll Pow(ll a, ll b) {
 	ll res = 1;
 	for (; b; b >>= 1, a = a * a % MOD) {
 		if (b & 1) {
@@ -14,24 +13,23 @@ ll pow(ll a, ll b) {
 		}
 	}
 	return res;
-
 }
-
-ll Fac[SZ + 1], Inv[SZ + 1];
 
 int main() {
 
 	scanf("%d%d", &N, &K);
 
-	Fac[0] = 1;
-	for (int i = 1; i <= N; i++) {
-		Fac[i] = Fac[i - 1] * i % MOD;
-	}
-	Inv[N] = pow(Fac[N], MOD - 2);
-	for (int i = N - 1; i >= 0; i--) {
-		Inv[i] = Inv[i + 1] * (i + 1) % MOD;
+	ll n = 1, k = 1;
+	for (int i = 0; i < K; i++) {
+		n *= N - i;
+		k *= K - i;
+
+		n %= MOD;
+		k %= MOD;
 	}
 
-	ll ans = Fac[N] * Inv[K] % MOD * Inv[N - K] % MOD;
+	k = Pow(k, MOD - 2);
+
+	ll ans = n * k % MOD;
 	printf("%ld", ans);
 }
